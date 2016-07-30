8da98b0004589f9cf9ac6820f1acb6af87104ec6
Benobab <benlacroix83@gmail.com>
2016-07-30 18:45:50 +0000

    refactor

--- server.js
+++ server.js
@@ -1,26 +1,36 @@
-var express = require("express");
-var path = require("path");
-var multer = require("multer");
-var bodyParser = require('body-parser');
+var express = require('express');
+var multer = require('multer'),
+	bodyParser = require('body-parser'),
+	path = require('path');
 
-var upload = multer({dest:'uploads/'});
-var app = express();
+var app = new express();
 
-app.use(bodyParser.json());
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
-
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname + '/views/index.html'));
+app.get('/', function(req, res){
+  res.render('index');
 });
 
-app.post('/analyse',upload.single('upl'),function(req,res,next){
-    console.log("A file has been sent ");
-    var file = req.file;
-    console.log(file);
-    console.log(req.body);
-    res.end(JSON.stringify({"size":file.size}));
+app.post('/analyse', multer({ dest: './uploads/'}).single('upl'), function(req,res){
+	console.log(req.body); //form fields
+	/* example output:
+	{ title: 'abc' }
+	 */
+	console.log(req.file); //form files
+	/* example output:
+            { fieldname: 'upl',
+              originalname: 'grumpy.png',
+              encoding: '7bit',
+              mimetype: 'image/png',
+              destination: './uploads/',
+              filename: '436ec561793aa4dc475a88e84776b1b9',
+              path: 'uploads/436ec561793aa4dc475a88e84776b1b9',
+              size: 277056 }
+	 */
+	res.end(JSON.stringify({"size":req.file.size}));
 });
 
-app.listen(process.env.PORT || '8080',function(){
-    console.log("App Running");
-});
+var port = 8080;
+app.listen( port, function(){ console.log('listening on port '+port); } );
