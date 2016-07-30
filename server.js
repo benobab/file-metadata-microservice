076b1ed277e0a7560ba4a762524a7e808abdd8f1
Benobab <benlacroix83@gmail.com>
2016-07-30 18:40:37 +0000

    ignore uploads

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 var express = require("express");
 var path = require("path");
 var multer = require("multer");
+var bodyParser = require('body-parser');
 
 var upload = multer({dest:'uploads/'});
 var app = express();
+
+app.use(bodyParser.json());
+
 
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname + '/views/index.html'));
 });
 
-app.post('/analyse',upload.single('filechosen'),function(req,res,next){
+app.post('/analyse',upload.single('upl'),function(req,res,next){
+    console.log("A file has been sent ");
     var file = req.file;
     console.log(file);
     console.log(req.body);
