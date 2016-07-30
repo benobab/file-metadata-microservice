28aa525b99102e5c693aa5124c5ca09aa46b9362
Benobab <benlacroix83@gmail.com>
2016-07-30 12:10:23 +0000

    one last error to fix

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 var express = require("express");
 var path = require("path");
+var multer = require("multer");
 
+var upload = multer({dest:'uploads/'});
 var app = express();
 
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname + '/views/index.html'));
 });
 
-app.post('/analyse',function(req,res){
-    
+app.post('/analyse',upload.single('filechosen'),function(req,res,next){
+    var file = req.file;
+    console.log(file);
+    console.log(req.body);
+    res.end(JSON.stringify({"size":file.size}));
 });
 
 app.listen(process.env.PORT || '8080',function(){
